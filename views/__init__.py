6d9359df575a984f7c17830f809ca565069eeb7e
Destiny Frith <desilyvahn@gmail.com>
2022-04-06 15:54:42 -0500

    data added and fetch calls working

---
+++ views/__init__.py
@@ -0,0 +1,4 @@
+from .animal_requests import get_all_animals
+from .location_requests import get_all_locations
+from .customer_requests import get_all_customers
+from .employee_requests import get_all_employees
