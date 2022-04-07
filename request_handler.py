89008640633cc7fb07b41259b8dadda6ee4cdbae
Destiny Frith <desilyvahn@gmail.com>
2022-04-07 14:58:19 -0500

    put method added

--- request_handler.py
+++ request_handler.py
@@ -1,8 +1,8 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from views.animal_requests import get_all_animals, get_single_animal, create_animal, delete_animal
-from views.location_requests import delete_location, get_all_locations, get_single_location, create_location, delete_location
-from views.customer_requests import get_all_customers, get_single_customer, create_customer, delete_customer
-from views.employee_requests import get_all_employees, get_single_employee, create_employee, delete_employee
+from views.animal_requests import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal
+from views.location_requests import get_all_locations, get_single_location, create_location, delete_location, update_location
+from views.customer_requests import get_all_customers, get_single_customer, create_customer, delete_customer, update_customer
+from views.employee_requests import get_all_employees, get_single_employee, create_employee, delete_employee, update_employee
 import json
 
 
@@ -158,9 +158,32 @@ class HandleRequests(BaseHTTPRequestHandler):
     # It handles any PUT request.
 
     def do_PUT(self):
-        """Handles PUT requests to the server
-        """
-        self.do_POST()
+        self._set_headers(204)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            update_animal(id, post_body)
+
+        # Delete a single customer from the list
+        if resource == "customers":
+            update_customer(id, post_body)
+
+            # Delete a single customer from the list
+        if resource == "employees":
+            update_employee(id, post_body)
+
+            # Delete a single customer from the list
+        if resource == "locations":
+            update_location(id, post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
 
     def do_DELETE(self):
         # Set a 204 response code
