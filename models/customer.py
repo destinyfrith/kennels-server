3375b3c4f6f7794d2ca503bf7ab92411ed68879d
Destiny Frith <desilyvahn@gmail.com>
2022-04-08 15:36:40 -0500

    imported sql and started querying resources finished with animal and customer

--- models/customer.py
+++ models/customer.py
@@ -5,10 +5,9 @@ class Customer():
     # Class initializer. It has 5 custom parameters, with the
     # special `self` parameter that every method on a class
     # needs as the first parameter.
-    def __init__(self, id, first_name, last_name, email, password, verify_password):
+    def __init__(self, id, name, address, email, password):
         self.id = id
-        self.first_name = first_name
-        self.last_name = last_name
+        self.name = name
+        self.address = address
         self.email = email
         self.password = password
-        self.verify_password = verify_password
