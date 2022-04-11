f11284e681686a08f44b20f5ff0e6da5bcbc19f3
Destiny Frith <desilyvahn@gmail.com>
2022-04-11 15:36:41 -0500

    added UPDATE functionality

--- request_handler.py
+++ request_handler.py
@@ -1,6 +1,6 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from views.animal_requests import get_all_animals, get_single_animal, get_animals_by_location, get_animals_by_status
+from views.animal_requests import get_all_animals, get_single_animal, get_animals_by_location, get_animals_by_status, delete_animal
 from views.location_requests import get_all_locations, get_single_location
 from views.customer_requests import get_all_customers, get_single_customer, get_customers_by_email
 from views.employee_requests import get_all_employees, get_single_employee, get_employees_by_location
@@ -176,29 +176,23 @@ class HandleRequests(BaseHTTPRequestHandler):
     # It handles any PUT request.
 
     def do_PUT(self):
+    content_len = int(self.headers.get('content-length', 0))
+    post_body = self.rfile.read(content_len)
+    post_body = json.loads(post_body)
+
+    # Parse the URL
+    (resource, id) = self.parse_url(self.path)
+
+    success = False
+
+    if resource == "animals":
+        success = update_animal(id, post_body)
+    # rest of the elif's
+
+    if success:
         self._set_headers(204)
-        content_len = int(self.headers.get('content-length', 0))
-        post_body = self.rfile.read(content_len)
-        post_body = json.loads(post_body)
-
-        # Parse the URL
-        (resource, id) = self.parse_url(self.path)
-
-        # Delete a single animal from the list
-        if resource == "animals":
-            update_animal(id, post_body)
-
-        # Delete a single customer from the list
-        if resource == "customers":
-            update_customer(id, post_body)
-
-            # Delete a single customer from the list
-        if resource == "employees":
-            update_employee(id, post_body)
-
-            # Delete a single customer from the list
-        if resource == "locations":
-            update_location(id, post_body)
+    else:
+        self._set_headers(404)
 
         # Encode the new animal and send in response
         self.wfile.write("".encode())
--- views/animal_requests.py
+++ views/animal_requests.py
@@ -143,3 +143,42 @@ def get_animals_by_status(status):
             animals.append(animal.__dict__)
 
     return json.dumps(animals)
+
+
+def delete_animal(id):
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        DELETE FROM animal
+        WHERE id = ?
+        """, (id, ))
+
+
+def update_animal(id, new_animal):
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        UPDATE Animal
+            SET
+                name = ?,
+                breed = ?,
+                status = ?,
+                location_id = ?,
+                customer_id = ?
+        WHERE id = ?
+        """, (new_animal['name'], new_animal['breed'],
+              new_animal['status'], new_animal['locationId'],
+              new_animal['customerId'], id, ))
+
+        # Were any rows affected?
+        # Did the client send an `id` that exists?
+        rows_affected = db_cursor.rowcount
+
+    if rows_affected == 0:
+        # Forces 404 response by main module
+        return False
+    else:
+        # Forces 204 response by main module
+        return True
